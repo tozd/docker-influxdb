bd732030deee2655b2b1789f41293b8efde351e2
Mitar <mitar.git@tnode.com>
2023-04-28 15:23:03 +0200

    Improve script.

--- test.sh
+++ test.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+set -e
+
+cleanup() {
+  echo "Stopping Docker image"
+  docker stop test
+}
+
 echo "Running Docker image"
-docker run -d --name test --rm -p 8086:8086 "${CI_REGISTRY_IMAGE}:${TAG}" || exit 1
+docker run -d --name test --rm -p 8086:8086 "${CI_REGISTRY_IMAGE}:${TAG}"
+trap cleanup EXIT
 
 echo "Sleeping"
 sleep 10
 
 echo "Testing"
 wget -q -S -O - http://docker:8086/ping 2>&1 | grep -q X-Influxdb-Version
-result=$?
-
-echo "Stopping Docker image"
-docker stop test || exit 2
-
-exit "$result"
